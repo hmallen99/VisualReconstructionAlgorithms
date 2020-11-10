c8d342db638b894a7d00b7ebd178a04834049137
Ansh Verma <52272280+123453ANSH@users.noreply.github.com>
2020-11-10 11:38:19 -0800

    Update orientationmaps2.py
    Successfully generates 8 depth maps

--- orientationmaps2.py
+++ orientationmaps2.py
@@ -1,4 +1,9 @@
-##########
+import numpy as np
+import cv2 as cv
+import sys
+import matplotlib.pyplot as plt
+import math
+
 img = cv.imread('TestImg.jpg')
 laplacian = cv.Laplacian(img,cv.CV_64F)
 
@@ -8,6 +13,12 @@ sobely1 = cv.Sobel(img,cv.CV_64F,0,1,ksize=5)
 sobelx2 = cv.Sobel(img,cv.CV_64F,1,0,ksize=5)
 sobely2 = cv.Sobel(img,cv.CV_64F,0,1,ksize=5)
 
+"""second set of four directions"""
+sobelx3 = cv.Sobel(img,cv.CV_64F,1,0,ksize=5, scale=math.sqrt(2)/2) + cv.Sobel(img,cv.CV_64F,0,1,ksize=5, scale=math.sqrt(2)/2)
+sobely3 = cv.Sobel(img,cv.CV_64F,1, 0,ksize=5, scale=-math.sqrt(2)/2) + cv.Sobel(img,cv.CV_64F,0,1,ksize=5, scale=math.sqrt(2)/2)
+sobelx4 = cv.Sobel(img,cv.CV_64F,1, 0,ksize=5, scale=-math.sqrt(2)/2) + cv.Sobel(img,cv.CV_64F,0, 1,ksize=5, scale=-math.sqrt(2)/2)
+sobely4 = cv.Sobel(img,cv.CV_64F,1, 0,ksize=5, scale=math.sqrt(2)/2) + cv.Sobel(img,cv.CV_64F,0, 1,ksize=5, scale=-math.sqrt(2)/2)
+
 plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
@@ -16,51 +27,18 @@ plt.subplot(2,2,3),plt.imshow(sobelx1,cmap = 'gray')
 plt.title('Sobel X1'), plt.xticks([]), plt.yticks([])
 plt.subplot(2,2,4),plt.imshow(sobely1,cmap = 'gray')
 plt.title('Sobel Y1'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,3),plt.imshow(sobelx2,cmap = 'gray')
-plt.title('Sobel X2'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,4),plt.imshow(sobely2,cmap = 'gray')
-plt.title('Sobel Y2'), plt.xticks([]), plt.yticks([])
-
-
-"""second set of four directions"""
-sobelx3 = cv.Sobel(img,cv.CV_64F,1,1,ksize=5, scale=math.sqrt(2)/2)
-#sobely3 = cv.Sobel(img,cv.CV_64F,-1,-1,ksize=5, scale=math.sqrt(2)/2)
-#sobelx4 = cv.Sobel(img,cv.CV_64F,1,-1,ksize=5, scale=math.sqrt(2)/2)
-#sobely4 = cv.Sobel(img,cv.CV_64F,-1,1,ksize=5, scale=math.sqrt(2)/2)
-
-
-
-plt.subplot(2,2,4),plt.imshow(sobelx3,cmap = 'gray')
-plt.title('Sobel X3'), plt.xticks([]), plt.yticks([])
-#plt.subplot(2,2,4),plt.imshow(sobely3,cmap = 'gray')
-#plt.title('Sobel Y3'), plt.xticks([]), plt.yticks([])
-#plt.subplot(2,2,4),plt.imshow(sobelx4,cmap = 'gray')
-#plt.title('Sobel X4'), plt.xticks([]), plt.yticks([])
-#plt.subplot(2,2,4),plt.imshow(sobely4,cmap = 'gray')
-#plt.title('Sobel Y4'), plt.xticks([]), plt.yticks([])
 plt.show()
-##########
-"""
-f(x, y) = z where z is pixel intensity
-grad(z) = <del z/ del x, del z, del y> direction of steepest ascent, directional derivatives dot u where u = <x, y, z> and |u| = 1
-<1, 0>
-<0, 1>
-<-1, 0>
-<0, -1>
-<sqrt(2)/2, sqrt(2)/2>
-<-sqrt(2)/2, -sqrt(2)/2>
-<sqrt(2)/2, -sqrt(2)/2>
-<-sqrt(2)/2, sqrt(2)/2>
-"""
-
-
-############
-
-
-
-############
-
-
-
-
-##############
+plt.subplot(2,2,1),plt.imshow(sobelx2,cmap = 'gray')
+plt.title('Sobel X2'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,2),plt.imshow(sobely2,cmap = 'gray')
+plt.title('Sobel Y2'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,3),plt.imshow(sobelx3,cmap = 'gray')
+plt.title('Sobel X3'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,4),plt.imshow(sobely3,cmap = 'gray')
+plt.title('Sobel Y3'), plt.xticks([]), plt.yticks([])
+plt.show()
+plt.subplot(2,2,1),plt.imshow(sobelx4,cmap = 'gray')
+plt.title('Sobel X4'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,2),plt.imshow(sobely4,cmap = 'gray')
+plt.title('Sobel Y4'), plt.xticks([]), plt.yticks([])
+plt.show()
